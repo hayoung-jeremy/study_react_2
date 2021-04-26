b8751fec3a499772506e25060f5f1a57d2cda0b9
Ha young, Kim <jeremiah91@naver.com>
2021-04-26 17:22:29 +0900

    connecting MongoDB

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express();
 // local port 설정, 번호는 아무거나 사용 가능
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://hayoungKim:1568978*kim@reactstudy2.tbqza.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
